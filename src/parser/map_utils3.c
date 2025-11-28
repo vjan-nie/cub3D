9ed170b9437610d7f180c18f01cbcb7db9aed256
serjimen <serjimen@student.42madrid.com>
2025-11-28 19:01:48 +0100

    [cub3d][Refactor]
    Refactor parcial y Norma parcial

--- src/parser/map_utils3.c
+++ src/parser/map_utils3.c
@@ -6,7 +6,7 @@
 /*   By: sergio-jimenez <sergio-jimenez@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/27 11:19:06 by sergio-jime       #+#    #+#             */
-/*   Updated: 2025/11/28 18:58:56 by sergio-jime      ###   ########.fr       */
+/*   Updated: 2025/11/28 19:00:52 by sergio-jime      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,11 +68,4 @@ int	skip_config(char **lines)
 			return (i);
 	}
 	return (i);
-}
-
-bool	is_valid_char(char c)
-{
-	if (c == '1' || c == '0' || c == 'N' || c == 'S' || c == 'E' || c == 'W')
-		return (true);
-	return (false);
 }
